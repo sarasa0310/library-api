0b71b218a6cf5aa82244a85862c2bad9d229968d
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-11 11:16:15 +0900

    Chore: 서버 포트 5000으로 변경 및 불필요한 연습 코드 삭제

--- src/main/java/com/codestates/excercise/item/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codestates.excercise.item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- src/main/java/com/codestates/excercise/order/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codestates.excercise.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
